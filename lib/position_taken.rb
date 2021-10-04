b307ef4e010158fa6128dc491d97c057ccffc52e
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-10-04 17:02:44 -0400

    commit tree 2e253078c9c1bbb3af0914d1b55b466a38e4a81d

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+    move = board[index]
+    if move == " " || move == "" || move == nil
+        taken = false
+    else
+        taken = true
+    end
+
+    taken
+end
